3864913f97220081bb56c386804b38e99e45cc4b
pragna majji <pragnajobseeker@gmail.com>
2018-05-29 09:41:28 +0530

    Temporary fix for save object

--- RedBracketConnector/RBConnector.cs
+++ RedBracketConnector/RBConnector.cs
@@ -43,10 +43,10 @@ namespace RedBracketConnector
             try
             {
                 //To unlock File in case of update
-                if (UnlockObject(plmobjs))
-                {
-                    return false;
-                }
+                //if (UnlockObject(plmobjs))
+                //{
+                //    return false;
+                //}
 
                 foreach (PLMObject obj in plmobjs)
                 {
@@ -54,11 +54,11 @@ namespace RedBracketConnector
                     SaveFileCommand objSFC = new SaveFileCommand();
 
                     // to convert file in bytes.
-                    using (var binaryReader = new BinaryReader(File.OpenRead(FilePath)))
-                    {
-                        binaryReader.Read();
-                        objSFC.BRDocument = binaryReader;
-                    }
+                    //using (var binaryReader = new BinaryReader(File.OpenRead(FilePath)))
+                    //{
+                    //    binaryReader.Read();
+                    //    objSFC.BRDocument = binaryReader;
+                    //}
 
                     //Defining parameters
                     KeyValuePair<string, string> L = new KeyValuePair<string, string>("project", obj.ObjectProjectId);
@@ -70,7 +70,7 @@ namespace RedBracketConnector
                     RestResponse restResponse = (RestResponse)ServiceHelper.SaveObject(
                         Helper.GetValueRegistry("LoginSettings", "Url").ToString(),
                         "/AutocadFiles/uploadFileService", obj.FilePath,
-                        obj.NativeFileName, true, new List<KeyValuePair<string, string>> {
+                        "test.dwg", true, new List<KeyValuePair<string, string>> {
                             new KeyValuePair<string, string>("project", obj.ObjectProjectId)
                 });
 
